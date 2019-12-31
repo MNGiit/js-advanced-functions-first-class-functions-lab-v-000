cc1a41196c45888661c19384a47091436def3ddd
Mohammed Noori <kmanoorib@gmail.com>
2019-12-31 04:12:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,17 @@ const returnFirstTwoDrivers = function(drivers) { return drivers.slice(0,2) };
 // const returnLastTwoDrivers = function(drivers) {
 //  return drivers.slice(-2)
 // }
-
 const returnLastTwoDrivers = function(drivers) { return drivers.slice(-2) };
 
+
+// const selectingDrivers = [one, two];
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
+// const createFareMultiplier = function(num1) {
+  // return function(num2) {
+    // return num1 * num2;
+  // }
+// }
 const createFareMultiplier = function(num1) { return function(num2) {return num1 * num2} };
 
 const fareDoubler = createFareMultiplier(2);
